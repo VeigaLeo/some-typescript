79256291bab0b7342bb3dcf5882e70519f6ea875
veigaleo <leo.veiiga@gmail.com>
2020-07-01 20:51:46 -0300

    sorting implementation

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("hi there");
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sort = new Sorter([5, 6, 1, 18, 54, 24]);
+sort.sort();
+console.log(sort.collection);
